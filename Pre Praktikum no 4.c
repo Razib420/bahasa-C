622f80ba58d34b744e485ed3f9dc5306c2266338
Razib420 <55045480+Razib420@users.noreply.github.com>
2019-09-24 22:49:46 +0700

    Add files via upload

---
+++ Pre Praktikum no 4.c
@@ -0,0 +1,24 @@
+// ini nih kan salah code nya, kada diperbaiki pang soal nya cuman aku handak tau kayapa caranya program nya jalan
+#include <stdio.h>
+
+int main()
+{
+	int maxtine = 199;
+	
+	for(int i = 0; i < maxtine; i++)
+	{
+		
+		if(i % 3 = 0 & i % 5 = 0){
+			printf("FizzBuzz");
+		}else (i % 3 = 0){
+			printf("Fizz");
+		}else (i % 5 = 0){
+			printf("Buzz");
+		}else{
+			printf(i);
+		}
+		
+		printf("\n");
+	}
+	return 0;
+}
